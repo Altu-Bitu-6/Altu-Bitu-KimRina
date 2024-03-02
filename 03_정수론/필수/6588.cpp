f1747d30d97fb1add89b67ebd8f1cc28cf301323
ri-naa <rina288@ewhain.net>
2024-03-03 04:51:05 +0900

    [정수론] 3월 3일

---
+++ 03_정수론/필수/6588.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm> 
+#include <vector>
+
+/*
+b - a가 가장 큰 경우가 언제일지 잘 생각해보아요! 
+n = a + b를 만족시키는 두 소수들을 어디서부터 탐색하면 좋을까요?
+*/
+using namespace std;
+
+int getPrime() {
+
+}
+
+
+int main() {
+
+    int input;
+    cin >> input;
+
+    while(input != 0) {
+        getPrime();
+        cin >> input;
+    }
+}
