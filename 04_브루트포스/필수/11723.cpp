76d693b57a8635cbd8ab34fea2156653ab992f4f
ri-naa <rina288@ewhain.net>
2024-03-12 03:07:31 +0900

    [브루트포스] 3월 12일

--- 04_브루트포스/필수/11723.cpp
+++ 04_브루트포스/필수/11723.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 
 using namespace std;
 
-/*
-set이나 vector를 사용해 집합을 다뤄줘도 좋지만, 
-이번에는 수업에서 배운 비트마스킹을 사용해 각 원소의 유무를 관리해봅시다!
-*/
-
 int main() {
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int S = 0;
+    int M;
+    cin >> M;
+
+    for(int i=0; i<M; i++) {
+        int num;
+        string a;
+        cin >> a;
+
+        if(a == "add") {
+            cin >> num;
+            S |= (1 << num); //1로 바꾸기
+        }
+        else if(a == "remove") {
+            cin >> num;
+            S &= ~(1 << num); //0으로 바꾸기
+        }
+        else if(a == "check") {
+            cin >> num;
+            if (S & (1 << num)) { //숫자가 존재하면 1출력
+                cout << "1\n";
+            }
+            else { //0이면 0출력
+                cout << "0\n";
+            }
+        }
+        else if(a == "toggle") {
+            cin >> num;
+            if(S & (1 << num)) { //x가 있으면 제거
+                S &= ~(1 << num);
+            }
+            else { //없으면 x 추가
+                S |= (1 << num);
+            }
+        }
+        else if (a == "all") {
+            S = (1 << 21) - 1; //111 ... 111 넣기
+        }
+        else if (a == "empty") {
+            S = 0;
+        }
+    }
+
+    return 0;
 }
