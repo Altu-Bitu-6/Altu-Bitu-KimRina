e9de33f6828553f737cf90995ae37eb3965f8f42
ri-naa <rina288@ewhain.net>
2024-03-26 02:16:51 +0900

    [그리디] 3월 26일

---
+++ 06.그리디/필수/15662.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+/*
+톱니바퀴가 돌아가는 것에는 순서가 있는게 아닙니다!
+(한 톱니바퀴가 돌아가고 이후 다른 톱니바퀴의 극을 보지 않는다는 뜻) 
+이점에 유의해서 문제를 풀어보세요.
+*/
+
+int main() {
+    
+}
